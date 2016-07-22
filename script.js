7dd5718bad2f48297c72fe3f02ed0486102ab84b
acmp1 <clarissamirandap@gmail.com>
2016-07-22 13:28:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#bContacto").click(function(){
         //var x=$("#contacto").position();
         window.scrollTo("#contacto");
+        $('#contacto').addClass('active');
     });
     $("#bPropiedades").click(function(){
         /*var element       = $("#propiedades");
@@ -11,9 +12,11 @@ $(document).ready(function(){
         var offset = Math.min(elementHeight, windowHeight) + element.offset().top;
         $('html, body').animate({ scrollTop: offset }, 500);*/
         $(window).scrollTop($('#propiedades').offset().top);
+        $('#contacto').addClass('active');
     });
     $("#bInicio").click(function(){
         $(window).scrollTop($('#dvideo').offset().top);
+        $('#contacto').addClass('active');
     });
     });
 });
